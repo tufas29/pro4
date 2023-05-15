1a7710fc146704fa6393965102775d7d52fcc4c8
Shmuel Tufahi <tufas29@gmail.com>
2023-05-15 22:54:45 +0300

    add import user

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,5 +1,5 @@
 import Prompt from "@models/prompt"
-// import User from "@models/user"
+import User from "@models/user"
 import { connectToDB } from "@utils/database"
 
 export const GET = async (request) => {
